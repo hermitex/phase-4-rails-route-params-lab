5212feb6191d2d3c81e82ca1c72ce2877e1d3122
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-09-28 16:36:03 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
+    name = params[:name]? params[:name].capitalize : nil
     students = Student.all
+    found_student = Student.where(last_name: name).or(Student.where(first_name: name)) #bonus
     render json: students
   end
+
+
+  def show
+    id = params[:id]
+    student = Student.find(id)
+    render json: student
+  end
+
 
 end
